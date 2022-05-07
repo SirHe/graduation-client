1db72eb86be7439b18d9ceba66409106479feb4e
SirHe <61593477+SirHe@users.noreply.github.com>
2022-05-08 01:37:41 +0800

    添加网站声明

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
   <el-config-provider :locale="locale">
     <router-view />
   </el-config-provider>
+  <Statement v-model:visible="visible" title="温馨提示" />
 </template>
 
 <script setup>
 import store from './store'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import cn from 'element-plus/lib/locale/lang/zh-cn'
 import en from 'element-plus/lib/locale/lang/en'
+import Statement from './components/Statement.vue'
+
+const visible = ref(true)
+
 const locale = computed(() => (store.getters.language === 'ch' ? cn : en))
 </script>
 
---
+++ src/components/Statement.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-dialog v-model="visible" :title="title" width="30%" @close="onClose">
+    <p class="content">
+      <b>
+        该网站使用的图片和文章均仅作为个人学习的测试数据，如有侵权，请联系站长删除。（联系方式：15211661645）
+      </b>
+    </p>
+  </el-dialog>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+defineProps({
+  title: {
+    type: String
+  },
+  visible: {
+    type: Boolean
+  }
+})
+
+const emits = defineEmits(['update:visible'])
+const onClose = () => {
+  emits('update:visible', false)
+}
+</script>
+
+<style scoped lang="less">
+.content {
+  color: red;
+}
+</style>
